64df4c416143fdfd847102d190aa36147715ee30
Sam Ardis <ardis.productions@gmail.com>
2020-09-20 19:15:00 -0400

    Added fetch request async await, grabbing data

--- covid-19-app/src/App.js
+++ covid-19-app/src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          
+          setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="App">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        {/* Dropdown*/}
+        <FormControl className="app__dropdown">
+          <Select variant='outlined' value='abc'>
+            {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        {/* Header */}
+      </div>
+
     </div>
   );
 }
